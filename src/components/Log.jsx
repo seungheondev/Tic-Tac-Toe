b225106e856edc9d2538ca8be5f9c4b8ea208e2f
seungheondev <lsh38216@gmail.com>
2024-06-10 23:58:04 +0900

    画面ログ出力

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,11 @@
-export default function Log() {
-    return <ol id='log'>
-        
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.box.row}${turn.box.col}`}>
+          {turn.player} selected {turn.box.row}, {turn.box.col}
+        </li>
+      ))}
     </ol>
+  );
 }
